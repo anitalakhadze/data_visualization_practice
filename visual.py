b51afa1b65b1dba94b259a63caed54f962c6b631
atala15 <atala15@freeuni.edu.ge>
2020-07-02 12:12:46 +0400

    added visual representation of a histogram for showing rolling frequencies

--- visual.py
+++ visual.py
@@ -1,3 +1,4 @@
+import pygal
 from die import Die
 
 # create a D6
@@ -14,6 +15,14 @@ frequencies = []
 for value in range(1, die.num_sides + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
-    print("Frequency of " + str(value) + " is " + str(frequency))
 
-print(frequencies)
+# Visualize the results.
+hist = pygal.Bar()
+
+hist.title = "Results of rolling one D6 1000 times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')

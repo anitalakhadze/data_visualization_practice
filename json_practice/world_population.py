457ae40beb9a713483299ac058aa433400e1bd35
atala15 <atala15@freeuni.edu.ge>
2020-07-05 00:06:19 +0400

    styling world maps in pygal with RotateStyle

--- json_practice/world_population.py
+++ json_practice/world_population.py
@@ -1,5 +1,6 @@
 import json
 from pygal.maps.world import World
+from pygal.style import RotateStyle
 from country_codes import get_country_code
 
 # Load the data into a list.
@@ -30,7 +31,8 @@ for cc, pop in cc_populations.items():
 # See how many countries are in each level.
 print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
 
-wm = World()
+wm_style = RotateStyle('#336699')
+wm = World(style=wm_style)
 wm.title = 'World Population in 2010, by Country'
 wm.add('0-10m', cc_pops_1)
 wm.add('10m-1bn', cc_pops_2)

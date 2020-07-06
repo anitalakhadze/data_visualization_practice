3e8737d8b17d87ba0b09e6255bf84be162cd5af9
atala15 <atala15@freeuni.edu.ge>
2020-07-06 10:38:04 +0400

    added custom tooltips

--- working_with_api/python_repos.py
+++ working_with_api/python_repos.py
@@ -14,10 +14,14 @@ print("Total repositories:", response_dict['total_count'])
 # Explore information about the repositories.
 repo_dicts = response_dict['items']
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'] or "",
+        }
+    plot_dicts.append(plot_dict)
 
 # Make visualization.
 my_style = LS('#333366', base_style=LCS)
@@ -36,5 +40,5 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
